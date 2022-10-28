365df9cb18a325072672eabce772b84a9cb662a7
Visaguirre <110070120+Visaguirre@users.noreply.github.com>
2022-10-27 20:42:39 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <>
+      <Navigation></Navigation>
+      <Header></Header>
+      <Login></Login>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h3>Login</h3>
+  )
+}
+
+export default Header
